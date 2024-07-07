6f7f84e05c3b1db705b279e32bb7a25894c485d3
sufi-1 <133345424+sufi-1@users.noreply.github.com>
2024-07-07 06:28:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ from pytgcalls import PyTgCalls
 if os.path.exists(".env"):
     load_dotenv(".env")
 
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-SESSION = os.getenv("SESSION")
+API_ID = int(os.getenv("API_ID", "14739610"))
+API_HASH = os.getenv("API_HASH", "4bb54a9de1d7b5570ccae74c2689f223")
+SESSION = os.getenv("AgDg6JoADJZIY3ftYj0Mf0uOT5bqoUdjrPTIcJV1PDKA0GIVk12_5iWu0uScvWHjkXpNkNqUGnaXAGLRmZ48znqlW7EJqbYWYuxEgpw2IZW-Z9Iuj91TVtKBO13-rCVvPBwn9xtESTQOUtluSm1PNiEdxGh-CDPJBMsW6n2CTKnAIVXiPFE3kjaeLqoVc3fhbky1lgrF8RJkWCARJiySHRul4SsxwEmR058VqJr5attMIq3KB-YxXZ-JvvHMyY-elZg97iPAN_qXUrf8yhI4K4fbqlItmODWeLupMg4fNo-zkBvTGTGVgIHj8CpOjHv2zFtnliVWrPrDhc8BNQzG5KKhsjzpkAAAAAF6rBtIAA")
 HNDLR = os.getenv("HNDLR", "!")
 
 contact_filter = filters.create(
